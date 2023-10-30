e830ea9149b97ab11cb8cf65fd95aea3acdefd88
vclee-github <59876792+vclee-github@users.noreply.github.com>
2023-10-30 16:47:48 +0000

    Added detect probe and detect probe direction
    Method can roughly estimate the probe tip using the major axis of the ellipse.

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ import numpy as np
 import cv2 as cv
 import threading
 import time
-import random
-import matplotlib.pyplot as plt
 
 #Classes
 from slicescope import Slicescope
@@ -25,8 +23,6 @@ from pvcam import PVCAM
 #from condenser import Condenser
 from image_window import Image_window
 from image_process import Image_process
-
-from tip_search_hist import hist_top_coord, get_tip_coord, split_index_list
 
 
 def main():
@@ -83,200 +79,28 @@ def main():
             print(f'Click Coordinate = {img_wnd.click_coord}')
             img_wnd.click_coord_update()
 
-            img_proc.load_frame(img_wnd.frame)
-            img_proc.contour()
-            img_wnd.add_overlay(img_proc.img_list[-1].edge)
+            #img_proc.load_frame(img_wnd.frame)
+            #img_proc.contour()
+            #img_wnd.add_overlay(img_proc.img_list[-1].edge)
 
-        #Click once to get a list of centroid points and draw arrowedline from centroid to projected points.
 
-            arrow_pt_list = np.array([])
-            centroid_list = np.array([])
-            projection_list = np.array([])
+            maxEllipse,points = img_proc.detect_probe(img_wnd)
 
-            #Take window frame and invert colors for threshold imaging
-            blurred_img = cv.blur(img_wnd.frame,(5,5))
-            thresh_inv = cv.threshold(blurred_img, 150, 255, cv.THRESH_BINARY_INV)[1]  #150 is the best value 
+            (x1,y1),(x2,y2) = img_proc.detect_probe_direction(img_wnd, maxEllipse, points)
 
-            thresh_inv_edges = cv.Canny(thresh_inv, 30, 200)
-            #cv.imshow('Inverse Image',thresh_inv_edges)
-            #cv.waitKey(0)
+            if not (np.isnan(x2) or np.isnan(y2)):
+                img_proc.quadrant(img_wnd, x2, y2)
 
-            inverse_contours, inverse_hierarchy = cv.findContours(thresh_inv_edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            #inverse_contours, inverse_hierarchy = cv.findContours(thresh_inv_edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-            inverse_hierarchy = inverse_hierarchy[0]
-            print(f"Contours Hierarchy = {inverse_hierarchy}")
 
-            # Hierarchy = [Next, Previous, First Child, Parent]
-            #c[0] = inverse_contours, c[1] = inverse_hierarchy
-            #Check if 'Next' location in hierarchy (c[1]) is -1 which is the outer most contour. 
-            #All detected contours are not closed so there are no parent and children parameters for these contours. 
-            #Locations c[2] and c[3] are set to -1
-            outer_contours = [c[0] for c in zip(inverse_contours,inverse_hierarchy) if c[1][0] == -1]
 
-            blank = np.zeros(img_wnd.frame.shape[:2], dtype=np.uint8)
-            outer_contours_lines = cv.drawContours(blank, outer_contours, -1, (255,255,255), 1)
-            #cv.imshow('Outer Contours Lines',outer_contours_lines)
-            #cv.waitKey(0)
 
-            #Must have edge to feed into hough_lines_intersect
-            #outer_edge = cv.Canny(outer_contours_lines, 150, 200) #30 or 150 produce similar results.
-            outer_edge = cv.Canny(outer_contours_lines, 30, 200)
-            #cv.imshow('outer_edge',outer_edge)
-            #cv.waitKey(0)
 
-            #Draw arrow from the potential intersection of 2-4 contour lines
-            img_proc.houghline_p_list,img_proc.houghline_intersect_list = img_proc.hough_lines_intersect(outer_edge)
-
-            #print(img_proc.houghline_p_list)
-            #print(img_proc.houghline_intersect_list)
-
-            out_idx = []
-            filtered_intersect_list = []
-
-            if img_proc.houghline_p_list is not None:
-
-                for line in img_proc.houghline_p_list:
-                    x1,y1,x2,y2 = line[0]
-                    img_wnd.frame = cv.line(img_wnd.frame,(x1,y1),(x2,y2),(0,0,0),5)
-                    centroid_list = np.append(centroid_list,[x1,y1])
-                    centroid_list = np.append(centroid_list,[x2,y2])
-
-                    #Get the index of the projected points that are on or near the HoughlinesP
-                    for idx,point in enumerate(img_proc.houghline_intersect_list):
-                        x3,y3 = point
-
-                        #Calculate the distance of x3,y3 w.r.t. x1,y1 and x2,y2 of the line to see if it lies between these 2 points
-                        check = img_proc.is_between([x1,y1],[x3,y3],[x2,y2])
-                        
-                        #if x3,y3 lies between these 2 points, save the index
-                        if check == True:
-                            out_idx = np.append(out_idx,idx)     
-                        else:
-                            continue
-                
-                centroid_list = np.reshape(centroid_list,[-1,2])
-                centroid_average = np.mean(centroid_list,axis=0)
-
-                arrow_pt_list = np.append(arrow_pt_list,[centroid_average[0],centroid_average[1]])
-                #print(f"avg centroid = {arrow_pt_list}")
-
-                #Exclude the projected points on or near the HoughlinesP using the index
-                #print("Unique index list:")
-                unique_out_idx = list(set(out_idx))
-                #print(unique_out_idx)
-
-                #remove duplicates in img_proc.houghline_intersect_list
-                filtered_intersect_list = img_proc.houghline_intersect_list.copy()               
-
-                if unique_out_idx is not None:
-                    for index in sorted(unique_out_idx, reverse=True):
-                        index = int(index)  #index must be integer to pass into ndarray
-                        filtered_intersect_list = np.delete(filtered_intersect_list,index,axis=0)
-
-                #print("Houghlines Intersect List:")
-                #print(img_proc.houghline_intersect_list)
-                #print("Filtered Intersect List:")
-                #print(filtered_intersect_list)
-
-            if filtered_intersect_list is not None:
-                for point in filtered_intersect_list:
-                    pt_x,pt_y = point
-
-                    #x = img_wnd.size[1]
-                    #y = img_wnd.size[0]
-                    if pt_x > img_wnd.size[1] or pt_y > img_wnd.size[0] or pt_x < 0 or pt_y < 0:
-                            continue
-                    else:
-                        projection_list = np.append(projection_list,[pt_x,pt_y])
-
-                        #Format the projected points to be drawn on the screen
-                        #pt_x = int(pt_x)
-                        #pt_y = int(pt_y)
-                        #dots = cv.circle(img_wnd.frame,(pt_x,pt_y),radius=10,color=(0,0,0),thickness=10)
-
-                if not len(projection_list) == 0:
-                    projection_list = np.reshape(projection_list,[-1,2])
-
-                    #img_wnd.add_overlay(dots)
-
-                    k_centroid_list,k_sse_list = img_proc.k_means(projection_list)
-
-                    for k_point in k_centroid_list:
-                        k_x,k_y = k_point
-                        #Format and draw the k-means cluster points on the screen
-                        k_x = int(k_x) 
-                        k_y = int(k_y)
-                        dots = cv.circle(img_wnd.frame,(k_x,k_y),radius=10,color=(255,255,255),thickness=10)
-
-                        img_wnd.add_overlay(dots)
-
-                    k_centroid_list = np.reshape(k_centroid_list,[-1,2])
-                    k_centroid_average = np.mean(k_centroid_list,axis=0)
-
-                    #List all of the projected intersection points
-                    #print("List of projected points")
-                    #print(k_centroid_list)
-
-                    arrow_pt_list = np.append(arrow_pt_list,[k_centroid_average[0],k_centroid_average[1]])          
-
-            #Display arrow and location/angle
-            if not len(arrow_pt_list) == 0:
-                arrow_pt_list = np.reshape(arrow_pt_list,[-1,2])
-                print(f"Arrowed Line points = {arrow_pt_list}")
-
-                start_point = (int(arrow_pt_list[0][0]), int(arrow_pt_list[0][1]))
-                end_point = (int(arrow_pt_list[-1][0]), int(arrow_pt_list[-1][1]))
-                img_wnd.frame = cv.arrowedLine(img_wnd.frame,start_point,end_point,(0,0,0),thickness=10)
-                img_wnd.add_overlay(img_wnd.frame)
-
-                #Calculate angle
-                delta_y = int(arrow_pt_list[-1][1]) - int(arrow_pt_list[0][1])
-                delta_x = int(arrow_pt_list[-1][0]) - int(arrow_pt_list[0][0])
-
-                angle = np.arctan2(delta_y, delta_x)
-
-                while angle < 0:
-                    angle = angle + (2*np.pi)
-
-                #print(f"angle = {angle} ")  #clockwise from start point to end point
-                print(f"Clockwise angle = {np.rad2deg(angle)} deg")  #clockwise from start point to end point
-                print(f"Counter clockwise angle = {360 - np.rad2deg(angle)} deg") #counter-clockwise from start point to end point 
-
-                print("Direction and Line are calculated with respect to the start coordinate. Imagine a Horizon Line through the Starting Point")
-                print(f"start coord = {start_point}")
-                print(f"end coord = {end_point}")
-                print(f"Move by x = {delta_x}")
-                print(f"Move by y = {delta_y}")
-
-                #Rough estimate of Quadrant location based on projected intersection point.
-                #screen 1376 is in position 1 and is the X axis
-                #screen 1024 is in position 0 and is the Y axis
-
-                center_of_screen = [int(img_wnd.size[1]/2),int(img_wnd.size[0]/2)]
-                #print(f"center of screen = {center_of_screen}")
-                #print(f"x = {int(img_wnd.size[1])}, y={int(img_wnd.size[0])}")
-
-                #identify the quadrant the end point/tip is located
-                arrow_tip_x = int(arrow_pt_list[-1][0])
-                arrow_tip_y = int(arrow_pt_list[-1][1])
-
-                if arrow_tip_x > center_of_screen[0] and arrow_tip_y < center_of_screen[1]:
-                    print(f"arrow tip x = {arrow_tip_x}, arrow tip y = {arrow_tip_y}")
-                    print("Quadrant 1 (+x,+y)")
-                if arrow_tip_x < center_of_screen[0] and arrow_tip_y < center_of_screen[1]:
-                    print(f"arrow tip x = {arrow_tip_x}, arrow tip y = {arrow_tip_y}")
-                    print("Quadrant 2 (-x,+y)")
-                if arrow_tip_x < center_of_screen[0] and arrow_tip_y > center_of_screen[1]:
-                    print(f"arrow tip x = {arrow_tip_x}, arrow tip y = {arrow_tip_y}")
-                    print("Quadrant 3 (-x,-y)")
-                if arrow_tip_x > center_of_screen[0] and arrow_tip_y > center_of_screen[1]:
-                    print(f"arrow tip x = {arrow_tip_x}, arrow tip y = {arrow_tip_y}")
-                    print("Quadrant 4 (+x,-y)")
+            #img_wnd.add_overlay(img_wnd.frame)
 
         # Exit if img_wnd thread killed
         if not img_wnd.thread.is_alive():
             break
-
+    
     #Close and disconnect all equipment
 
     cam.disconnect()
